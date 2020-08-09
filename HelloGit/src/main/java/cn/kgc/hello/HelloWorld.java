02665692c2b3eb0cb8dd3dd0154d3cd847900808
zzhhbs <908924736@qq.com>
2020-08-09 22:38:23 +0800

    HelloWorld11 提交

---
+++ HelloGit/src/main/java/cn/kgc/hello/HelloWorld.java
@@ -0,0 +1,10 @@
+package cn.kgc.hello;
+
+/**
+ * Created by helloworld on 2020/8/9.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello11");
+    }
+}
