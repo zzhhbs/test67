e9ae3437e545fa0ab03f399621756eebcc87689d
zzhhbs <908924736@qq.com>
2020-08-09 23:33:17 +0800

    world

---
+++ Hello2/src/main/java/cn/kgc/world.java
@@ -0,0 +1,10 @@
+package cn.kgc;
+
+/**
+ * Created by helloworld on 2020/8/9.
+ */
+public class world {
+    public static void main(String[] args) {
+        System.out.println("hehhhh");
+    }
+}
