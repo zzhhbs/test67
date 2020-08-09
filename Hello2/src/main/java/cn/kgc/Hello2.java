751e2ee67b91231dbafb23375a38b974a4d2fdb5
zzhhbs <908924736@qq.com>
2020-08-09 23:03:53 +0800

    Hellot提交

---
+++ Hello2/src/main/java/cn/kgc/Hello2.java
@@ -0,0 +1,10 @@
+package cn.kgc;
+
+/**
+ * Created by helloworld on 2020/8/9.
+ */
+public class Hello2 {
+    public static void main(String[] args) {
+        System.out.println("hello2提交");
+    }
+}
